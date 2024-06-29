0cffec8b7c37225a373b3d6dd569f07ce118121f
MarcinTester <marcintester19@gmail.com>
2024-06-29 23:06:31 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,24 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  console.log(todo);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <span className="heading">Tasks</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList/> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,18 +1,30 @@
-import React from "react";
 import "./styles.css";
+import React, { useRef } from "react";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const inputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input 
-      type="input" 
-      value ={todo}
-      onChange={(e) => setTodo(e.target.value)}
-      placeholder="Enter a task" className="input_box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur()
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input_box"
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
@@ -20,4 +32,4 @@ const inputField = ({ todo, setTodo }: Props) => {
   );
 };
 
-export default inputField;
+export default InputField;
